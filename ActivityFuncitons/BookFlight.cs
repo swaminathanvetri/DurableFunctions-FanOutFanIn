52594690cd86d10fc336ab4771391a5744aeda69
Amresh Krishnamurthy <amresh.kris@gmail.com>
2020-05-02 21:23:10 +0530

    Uncommented the code that was used to demostrate error handling scenarios

--- ActivityFuncitons/BookFlight.cs
+++ ActivityFuncitons/BookFlight.cs
@@ -9,29 +9,29 @@ namespace Bdotnet.Demo
     public static class BookFlight
     {
 
-        // [FunctionName("BookFlightTicket")]
-        // public static async Task<double> ActivityBookFlight([ActivityTrigger] VacationInfo vacationInfo, ILogger log)
-        // {            
-        //      log.LogInformation($"Flight ticket booked for {vacationInfo.cityName} with the airlines {vacationInfo.airlinesName}");        
-        //     return await Task.FromResult(10000);
-        // }
-
-
         [FunctionName("BookFlightTicket")]
-        public static async Task<double> BookFlightTotheCountry([ActivityTrigger] VacationInfo vacationInfo, ILogger log)
-        {
-            try
-            {
-                await Task.Run(() => throw new Exception("some exception happened"));
-            }
-            catch (System.Exception ex)
-            {
-                log.LogError($"Error in booking {vacationInfo.airlinesName} {ex}");
-               
-            }
-
-            return 0.0;
+        public static async Task<double> ActivityBookFlight([ActivityTrigger] VacationInfo vacationInfo, ILogger log)
+        {            
+             log.LogInformation($"Flight ticket booked for {vacationInfo.cityName} with the airlines {vacationInfo.airlinesName}");        
+            return await Task.FromResult(10000);
         }
+
+
+        // [FunctionName("BookFlightTicket")]
+        // public static async Task<double> BookFlightTotheCountry([ActivityTrigger] VacationInfo vacationInfo, ILogger log)
+        // {
+        //     try
+        //     {
+        //         await Task.Run(() => throw new Exception("some exception happened"));
+        //     }
+        //     catch (System.Exception ex)
+        //     {
+        //         log.LogError($"Error in booking {vacationInfo.airlinesName} {ex}");
+               
+        //     }
+
+        //     return 0.0;
+        // }
     }
 }
 
--- OrchestratorFunction/BookVacationOrchestrator.cs
+++ OrchestratorFunction/BookVacationOrchestrator.cs
@@ -13,29 +13,6 @@ namespace Bdotnet.Demo
 
         // #region "Book Vacation Orchestrator"
 
-        // [FunctionName("BookVacation")]
-        // public static async Task<double> RunOrchestrator(
-        //     [OrchestrationTrigger] IDurableOrchestrationContext context,
-        //     ILogger log)
-        // {
-        //     //Receive Input
-        //     var vacationDetails = context.GetInput<VacationInfo>();
-
-        //     //Call Activity Functions in parallel
-        //     var bookHotel = context.CallActivityAsync<double>("BookHotel", vacationDetails.hotelName);
-        //     var bookTransport = context.CallActivityAsync<double>("BookFlightTicket", vacationDetails);
-
-        //     //Wait for Acitivity Functions to complete - Fan-In
-        //     var bookingTasks = (await Task.WhenAll(bookHotel, bookTransport)).ToList();
-
-        //     //Call another activity function to compute total
-        //     return await context.CallActivityAsync<double>("ComputeTotalExpense", bookingTasks);
-        // }
-
-        // #endregion
-
-        #region "Orchestrator Funciton - BookVacationWithErrorAndRetry"
-
         [FunctionName("BookVacation")]
         public static async Task<double> RunOrchestrator(
             [OrchestrationTrigger] IDurableOrchestrationContext context,
@@ -43,35 +20,58 @@ namespace Bdotnet.Demo
         {
             //Receive Input
             var vacationDetails = context.GetInput<VacationInfo>();
-            List<double> bookingTasks = new List<double>();
 
             //Call Activity Functions in parallel
             var bookHotel = context.CallActivityAsync<double>("BookHotel", vacationDetails.hotelName);
             var bookTransport = context.CallActivityAsync<double>("BookFlightTicket", vacationDetails);
 
-            try
-            {
-                //Wait for Acitivity Functions to complete - Fan-In
-                await Task.WhenAll(bookHotel, bookTransport);
-                
-            }
-            catch (System.Exception)
-            {
-                if (bookTransport.IsFaulted)
-                {
-                    log.LogError("Exception in bookng flight ticker. Please try later");
-                    bookingTasks.Add(bookHotel.Result);
-                }
-            }
+            //Wait for Acitivity Functions to complete - Fan-In
+            var bookingTasks = (await Task.WhenAll(bookHotel, bookTransport)).ToList();
 
-            //Call another Activity Function
-            var retryOptions = new RetryOptions(
-                firstRetryInterval: TimeSpan.FromSeconds(5),
-                maxNumberOfAttempts: 5);
-
-            return await context.CallActivityWithRetryAsync<double>("ComputeTotalExpense", retryOptions, bookingTasks);
-
+            //Call another activity function to compute total
+            return await context.CallActivityAsync<double>("ComputeTotalExpense", bookingTasks);
         }
+
+        // #endregion
+
+        #region "Orchestrator Funciton - BookVacationWithErrorAndRetry"
+
+        // [FunctionName("BookVacation")]
+        // public static async Task<double> RunOrchestrator(
+        //     [OrchestrationTrigger] IDurableOrchestrationContext context,
+        //     ILogger log)
+        // {
+        //     //Receive Input
+        //     var vacationDetails = context.GetInput<VacationInfo>();
+        //     List<double> bookingTasks = new List<double>();
+
+        //     //Call Activity Functions in parallel
+        //     var bookHotel = context.CallActivityAsync<double>("BookHotel", vacationDetails.hotelName);
+        //     var bookTransport = context.CallActivityAsync<double>("BookFlightTicket", vacationDetails);
+
+        //     try
+        //     {
+        //         //Wait for Acitivity Functions to complete - Fan-In
+        //         await Task.WhenAll(bookHotel, bookTransport);
+                
+        //     }
+        //     catch (System.Exception)
+        //     {
+        //         if (bookTransport.IsFaulted)
+        //         {
+        //             log.LogError("Exception in bookng flight ticker. Please try later");
+        //             bookingTasks.Add(bookHotel.Result);
+        //         }
+        //     }
+
+        //     //Call another Activity Function
+        //     var retryOptions = new RetryOptions(
+        //         firstRetryInterval: TimeSpan.FromSeconds(5),
+        //         maxNumberOfAttempts: 5);
+
+        //     return await context.CallActivityWithRetryAsync<double>("ComputeTotalExpense", retryOptions, bookingTasks);
+
+        // }
         #endregion
     }
 }
